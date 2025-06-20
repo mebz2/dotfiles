a2e6d5d561524540cd2c85d95e9e9735efa12237
mebz2 <zelalemmebea@gmail.com>
2025-06-20 19:56:33 +0300

    tmux config added

--- create_links.sh
+++ create_links.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf "$HOME/.config/hypr" "$HOME/.config/waybar" "$HOME/.config/rofi" "$HOME/.config/nvim"  
+rm -rf "$HOME/.config/hypr" "$HOME/.config/waybar" "$HOME/.config/rofi" "$HOME/.config/nvim"  "$HOME/.tmux.conf" 
 
 ln -s "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
 ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 ln -s "$HOME/dotfiles/rofi" "$HOME/.config/rofi"
 ln -s "$HOME/dotfiles/waybar" "$HOME/.config/waybar"
 ln -s "$HOME/dotfiles/kitty/" "$HOME/.config/kitty"
+ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
