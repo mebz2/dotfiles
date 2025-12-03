ea06626adee4f9a224e4d5cdc4c58d1906493ba9
mebz <zelalemmebea@gmail.com>
2025-12-03 10:37:04 +0300

    command to go to the line you were in

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -17,3 +17,15 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "help",
 	command = "wincmd L", -- vertical split for help
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = vim.api.nvim_create_augroup("last_location", { clear = true }),
+	desc = "Go to the last location when opening a buffer",
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.cmd('normal! g`"zz')
+		end
+	end,
+})
